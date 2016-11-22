fcf950035bb46788146ce6863503e7cdbaca80c9
Ethan Allen <eaallen@iupui.edu>
2016-11-22 15:39:48 -0500

    versino 0.2.1

--- plugin-options.php
+++ plugin-options.php
@@ -4,92 +4,69 @@
  * Class for building wicked easy plugin options pages.
  *
  * @link       http://endif.media
- * @since      1.0.0
+ * @since      0.2.1
  *
  * @package    Plugin_Options
- */
-
-/**
- * The admin-specific functionality of the plugin.
- *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the admin-specific stylesheet and JavaScript.
- *
- * @package    Post_Sync
- * @author     Ethan Allen <yourfriendethan@gmail.com>
+ *@author     Ethan Allen <yourfriendethan@gmail.com>
  */
 class Plugin_Options {
 
 	/**
-	 * The version of this plugin.
+	 * The nice name this plugin.
 	 *
-	 * @since    1.0.0
+	 * @since    0.2.1
 	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
+	 * @var      string    $version    The user readable name of this plugin.
 	 */
 	private $plugin_nicename;
 
 	/**
 	 * The plugin option name.
 	 *
-	 * @since    1.0.0
+	 * @since    0.1
 	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
+	 * @var      string    $version    The option name of this plugin.
 	 */
      private $plugin_option_name;
 
 	/**
-	 * The plugin settings.
+	 * The plugin menu page.
 	 *
-	 * @since    1.0.0
+	 * @since    0.2.1
 	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
+	 * @var      string    $version    The menu/settings page for this plugin.
 	 */
 	private $plugin_menu_page;
 
 	/**
 	 * Initialize the class and set its properties.
 	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
+	 * @since      0.1
+	 * @param      string    $plugin_nicename       User readable name of this plugin.
+	 * @param      string    $plugin_option_name    Option variable name you are using to store settings in the DB.
+	 * @param      string    $plugin_menu_page      Settings page slug where setting will display.
 	 */
 	public function __construct( $plugin_nicename, $plugin_option_name, $plugin_menu_page ) {
 
-		$this->plugin_nicename = $plugin_nicename;
-		$this->plugin_slug = sanitize_title($plugin_nicename);
-		$this->plugin_settings = get_option($plugin_option_name);
-		$this->plugin_option_name = $plugin_option_name;
-		$this->plugin_menu_page = $plugin_menu_page;
+		$this->plugin_slug_w_underscores = 'invoice_app';
+
+		$this->plugin_nicename = $plugin_nicename; //plugin nicename 'My Plugin'
+		$this->plugin_slug = sanitize_title($plugin_nicename);//slug version of plugin name  'my-plugin'
+		$this->plugin_settings = get_option($plugin_option_name);//current plugin settings
+		$this->plugin_option_name = $plugin_option_name;//plugin option name
+		$this->plugin_menu_page = $plugin_menu_page;//plugin menu page
 		
 	}
 
 	/**
-	 * Get tab title from navigation tabs.
+	 * Render navigation tabs.
 	 *
-	 * @since    1.0.0
-	 */
-	public function unslugify($active_tab, $tabs) {
-
-		$active_tab = str_replace('-', ' ', $active_tab);//replace dash with space
-		foreach($tabs as $tab){
-
-			if (strcasecmp($active_tab, $tab) == 0){
-				return $tab;
-			}
-		}
-
-	}
-
-	/**
-	 * Render settings navigation tabs.
-	 *
-	 * @since    1.0.0
+	 * @since    0.1
 	 */
 	public function render_tabs($tabs, $active_tab) {
 
 		$navtabs = '';
-		foreach ($tabs as $tab){
+		foreach ($tabs as $tab => $settings){
         	
         	$tab_slug = sanitize_title($tab);
         	$current  = ($tab_slug == sanitize_title($active_tab)) ? 'nav-tab-active' : '';
@@ -99,87 +76,128 @@ class Plugin_Options {
         			 . $tab_slug
         			 . '" class="nav-tab ' 
         			 . $current . '">' 
-        			 . $tab . '</a>';     
+        			 . ucwords($tab) . '</a>';     
 		}
-		return '<h2 class="nav-tab-wrapper">' . $navtabs . '</h2>';
+		echo '<h2 class="nav-tab-wrapper">' . $navtabs . '</h2>';
 
 	}
 
 	/**
-	 * Register the JavaScript for the admin area.
+	 * Render form fields.
 	 *
-	 * @since    1.0.0
+	 * @since    0.2.0
 	 */
-	public function render_fields($active_tab, $tabs, $options){
-		$fields ='';
-	    foreach ($options[$this->unslugify($active_tab, $tabs)] as $optionset) {
-        	$opt_label = $optionset[0];
-			$opt_name  = $optionset[1];
-			$opt_type  = $optionset[2];
-			$opt_values= $optionset[3];
-			$opt_note  = $optionset[4];
+	public function render_fields($active_tab, $options){
+		
+		$html ='';
+	    foreach ($options as $tab => $fields) {
 
-			$fields .= '<tr valign="top"><th scope="row">' . __($opt_label . " : ", '') . '</th><td>'; 
+			if ($active_tab == sanitize_title($tab)){
+				foreach ($fields as $field) {
 
-            switch ($opt_type) {
-                case 'text':
-                    $fields .= '<input type="text" name="'
-                                  . $opt_name .'" value="'
-                                  . esc_attr($this->plugin_settings[$opt_name]) 
-                                  . '" class="regular-text">
-                    ';
-                    if ($opt_note) 
-                    	$fields .= '<br><small>' . $opt_note . '</small>';
+					$html .= '<tr valign="top"><th scope="row">' . __($field['label'] , '') . '</th><td>'; 
 
-                    break;
-                case 'checkbox':
-                    $fields .= '<input type="checkbox" name="'
-                                  . $opt_name . '" value="1"'
-                                  . checked($this->plugin_settings[$opt_name], 1, false)//(($this->plugin_settings[$opt_name] === 'true') ? ' checked="checked"' : '')
-                                  . ' size="20">
-                    ';
-                    if ($opt_note) 
-                    	$fields .= '<small>' . $opt_note . '</small>';
+		            switch ($field['type']) {
+		                case 'text':
+		                    $size = ( isset( $field['size'] ) && ! is_null( $field['size'] ) ) ? $field['size'] : 'regular';
+		                    $html .= '<input type="text" name="'
+		                                  . $field['id'] .'" value="'
+		                                  . esc_attr($this->plugin_settings[$field['id']]) 
+		                                  . '" class="' . sanitize_html_class( $size ) . '-text">
+		                    ';
+		                    if ($field['desc']) 
+		                    		if (isset($size) && $size == 'small') {
+		                    	    	$html .= '<small>' . $field['desc'] . '</small>';
+		                    	    }else {
+		                    	    	$html .= '<br><small>' . $field['desc'] . '</small>';
+		                    	    }
 
-                    break;
+		                    break;
 
-                case 'select':
-                    $fields .= '<select name="'
-                                  . $opt_name . '" >';
-                                  foreach ($opt_values as $val) {
-                                    $fields .= '<option value="' . $val . '"' . selected($this->plugin_settings[$opt_name], $val, false) . '>' . $val . '</option>';
-                                  }
-                                  $fields .= '</select>
-                    ';
-                    if ($opt_note) 
-                    	$fields .= '<br><small>' . $opt_note . '</small>';
+		                case 'upload':
+		                    $size = ( isset( $field['size'] ) && ! is_null( $field['size'] ) ) ? $field['size'] : 'regular';
+		                    /*$html .= '<input type="text" name="'
+		                                  . $field['id'] .'" value="'
+		                                  . esc_attr($this->plugin_settings[$field['id']]) 
+		                                  . '" class="' . sanitize_html_class( $size ) . '-text">
+		                    ';
+		                    if ($field['desc']) 
+		                    	$html .= '<br><small>' . $field['desc'] . '</small>';*/
+		                    //$html .= '<label for="' . $field['id'] . '"> ' . $field['label'] . '</label>';
+		                    $html .= '<input type="text" class="' . sanitize_html_class( $size ) . '-text" id="' . $field['id'] . '" name="' . $field['id'] . '" value="' . esc_attr( $this->plugin_settings[$field['id']] ) . '"/>';
+							$html .= '<span>&nbsp;<input type="button" class="edd_settings_upload_button button-secondary" value="' . __( 'Upload File', $this->plugin_slug ) . '"/></span>';
 
-                    break;
 
-                case 'url':
-                    $fields .= '<input type="url" name="'
-                                  . $opt_name .'" value="'
-                                  . esc_attr($this->plugin_settings[$opt_name]) 
-                                  . '" class="regular-text code">
-                    ';
-                    if ($opt_note) 
-                    	$fields .= '<br><small>' . $opt_note . '</small>';
+		                    break;
+		            
+				        case 'checkbox':
+		                    $html .= '<input type="checkbox" name="'
+		                                  . $field['id'] . '" value="1"'
+		                                  . checked($this->plugin_settings[$field['id']], 1, false)//(($this->plugin_settings[$opt_name] === 'true') ? ' checked="checked"' : '')
+		                                  . ' size="20">
+		                    ';
+		                    if ($field['desc']) 
+		                    	$html .= '<small>' . $field['desc'] . '</small>';
 
-                    break;
-            }
+		                    break;
+
+		                case 'select':
+		                    $html .= '<select name="'
+		                                  . $field['id'] . '" >';
+		                                  foreach ($field['options'] as $value => $name) {
+		                                    $html .= '<option value="' . $value . '"' . selected($this->plugin_settings[$field['id']], $value, false) . '>' . $name . '</option>';
+		                                  }
+		                                  $html .= '</select>
+		                    ';
+		                    if ($field['desc']) 
+		                    	$html .= '<br><small>' . $field['desc'] . '</small>';
+
+		                    break;
+
+		                case 'url':
+		                    $html .= '<input type="url" name="'
+		                                  . $opt_name .'" value="'
+		                                  . esc_attr($this->plugin_settings[$field['id']]) 
+		                                  . '" class="regular-text code">
+		                    ';
+		                    if ($field['desc']) 
+		                    	$html .= '<br><small>' . $field['desc'] . '</small>';
+
+		                    break;
+
+		                case 'textarea':
+		                    $html .= '<textarea class="large-text" cols="50" rows="5" name="'
+		                                  . $opt_name .'" rows="4" cols="16" class="large-text">'
+		                                  . esc_attr($this->plugin_settings[$field['id']]) 
+		                                  . '</textarea>
+		                    ';
+		                    if ($field['desc']) 
+		                    	$html .= '<br><small>' . $field['desc'] . '</small>';
+
+		                    break;  
+
+
+		                    /*ob_start();
+			wp_editor( stripslashes( $value ), 'edd_settings_' . esc_attr( $args['id'] ), array( 'textarea_name' => 'edd_settings[' . esc_attr( $args['id'] ) . ']', 'textarea_rows' => absint( $rows ) ) );
+			$html = ob_get_clean();*/
+		            }
+		        }
+		    }
         }
-        return $fields;
+        echo $html;
 	}
 
 
 	/**
-	 * Register the JavaScript for the admin area.
+	 * Save form.
 	 *
-	 * @since    1.0.0
+	 * Function used to save plugin settings.
+	 *
+	 * @since    0.1
 	 */
-	public function save_form($active_tab, $tabs, $options){
+	public function save_form($active_tab, $options){
 
-	    foreach ($options[$this->unslugify($active_tab, $tabs)] as $optionset) {
+	    /*foreach ($options[$this->unslugify($active_tab, $tabs)] as $optionset) {
 			$opt_name  = $optionset[1];
 			$opt_type  = $optionset[2];
 
@@ -210,16 +228,76 @@ class Plugin_Options {
 
             update_option($this->plugin_option_name, $this->plugin_settings);
  
-        }
+        }*/
+
+        //for each option[current tab}
+        foreach ($options as $tab => $fields) { //tabs and fields
+
+			if ($active_tab == sanitize_title($tab)){
+				foreach ($fields as $field) {
+
+					switch ($field['type']) {
+		                case 'text':
+
+		                    $this->plugin_settings[$field['id']] = sanitize_text_field($_POST[$field['id']]);
+
+		                    break;
+
+		                case 'upload':
+		                    
+		                    $this->plugin_settings[$field['id']] = esc_url_raw($_POST[$field['id']]);
+
+		                    break;
+
+		                case 'checkbox':
+		                    
+		                    $this->plugin_settings[$field['id']] = intval($_POST[$field['id']]);
+
+		                    break;
+		                    
+						case 'select':
+		                    
+		                    $this->plugin_settings[$field['id']] = sanitize_text_field($_POST[$field['id']]);
+
+		                    break;
+
+		                case 'url':
+		                    //$opt_name = sanitize_
+		                    $this->plugin_settings[$field['id']] = esc_url_raw($_POST[$field['id']]);
+
+		                    break;
+
+		                case 'textarea':
+		                    //$opt_name = sanitize_
+		                    $this->plugin_settings[$field['id']] = sanitize_text_field($_POST[$field['id']]);
+
+		                    break;
+
+		                default:
+		            		# code...
+		                	$this->plugin_settings[$field['id']] = sanitize_text_field($_POST[$field['id']]);
+
+		            		break;
+		            }
+
+				}
+
+				update_option($this->plugin_option_name, $this->plugin_settings);
+
+			}
+
+		}
         echo '<div id="message" class="updated fade"><p>'. __('Options saved successfully.', sanitize_title($this->plugin_name) ) .'</p></div>';
 	}
 
 	/**
-	 * Register the JavaScript for the admin area.
+	 * Render form.
 	 *
-	 * @since    1.0.0
+	 * Function that handles the display of plugin settings page.
+	 *
+	 * @since    0.2.1
 	 */
-	public function render_form($tabs, $options, $active_tab) {
+	public function render_form($options, $active_tab) {
 		
 		if (isset($_POST[$this->plugin_slug . '_settings_save'])){
 
@@ -229,36 +307,28 @@ class Plugin_Options {
 				$this->plugin_slug . '_popts_name_of_nonce'
 			);
 
-			$this->save_form($active_tab, $tabs, $options);
+			$this->save_form($active_tab, $options);
 
-		}
+		} ?>
 
-		echo  '<div class="wrap">
-				   <h2>' . $this->plugin_nicename . '- settings' .'</h2>
-			       <div id="page-wrap">'
-			       	   . $this->render_tabs($tabs, $active_tab) .
-			       '<div id="inside">
-				    <div id="options-div">
-					   <form method="post" class="settings-form" style="margin-left: 15px">'
-					     . wp_nonce_field( $this->plugin_slug . '_popts_save_form', $this->plugin_slug . '_popts_name_of_nonce' )
-					     . '<fieldset class="options">
-					          <table class="form-table">'
-		        	            . $this->render_fields($active_tab, $tabs, $options) .
-		        	          '</table>
-						    </fieldset>
-						    <p><input type="submit" name="' 
-						      . $this->plugin_slug
-						      . '_settings_save" id="' 
-						      . $this->plugin_slug 
-						      . '_settings_save" class="button button-primary" value="Save Settings"  /></p>
-				       </form>
-					</div><!-- end options div -->
-				    </div><!-- end inside -->
-			     <div style="clear: both;"></div>
-			    </div><!-- end pagewrap -->
-		        </div><!-- end wrap -->';
-		        
+		<div class="wrap">
+		    <h2><?php echo $this->plugin_nicename; ?> - settings</h2>
+		    <?php $this->render_tabs($options, $active_tab); ?>
+		    <form method="post" action=""> 
+		    	<?php wp_nonce_field( $this->plugin_slug . '_popts_save_form', $this->plugin_slug . '_popts_name_of_nonce' ); ?>
+
+		        <fieldset class="options">
+					<table class="form-table">
+					<?php $this->render_fields($active_tab, $options); ?>
+					</table>
+			    </fieldset>
+
+		        <input type="submit" class="button button-primary" name="<?php echo $this->plugin_slug . '_settings_save'; ?>">
+		    </form>
+		</div>
+
+		<?php
+
 	}
-
 
 }
